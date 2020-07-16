4c0825e2c3f8cc0d656a5cc326b404beb1a460e4
Y.Arimitsu <yarimit@gmail.com>
2020-07-17 00:06:29 +0900

    Add Yolo Object

---
+++ Assets/Scripts/TinyYolo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Barracuda;
+
+public class TinyYolo : MonoBehaviour
+{
+    public NNModel modelAsset;
+    private Model m_RuntimeModel;
+    private IWorker m_worker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RuntimeModel = ModelLoader.Load(modelAsset);
+        m_worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Compute, m_RuntimeModel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
